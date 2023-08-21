513b45e75069a6614fa5beb9a41575e402c5ea88
Raj Dave <10864@bpsdoha.edu.qa>
2023-08-21 17:01:33 +0300

    add chatbot and main.py

---
+++ main.py
@@ -0,0 +1,27 @@
+import flask
+from chatbot_ import chatbot_
+
+app = flask.Flask(__name__, template_folder="html", static_folder='jscss')
+
+
+@app.route("/")
+def index():
+    return flask.render_template("index.html")
+
+
+@app.route("/chatbot", methods=["POST"])
+def chatbot():
+    print("e")
+    if flask.request.method == "POST":
+        user_input = flask.request.json["user_input"]
+        print(user_input)
+        resp = chatbot_.respond(user_input)
+        if not resp:
+            resp = "Sorry, I don't understand. Please try again."
+        return {'status': 'ok', 'response': resp}
+    else:
+        print(flask.request.method)
+
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=5000, debug=True)
