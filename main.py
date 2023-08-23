d635a1ad4dd4832134286223588fafab386b8f6c
Raj Dave <10864@bpsdoha.edu.qa>
2023-08-23 13:24:15 +0300

    minor change and add help.html to flask

--- main.py
+++ main.py
@@ -6,18 +6,18 @@ app = flask.Flask(__name__, template_folder="html", static_folder='static')
 
 @app.route("/")
 def index():
-    return flask.render_template("index.html", intro_message=intro_message)
+    return flask.render_template("index.html")
 
 
-intro_message = ("Hello! I am the Qatar Expo 2023 chatbot. Try asking me about  the venue, schedule, or participating "
-                 "countries.")
+@app.route("/help", methods=["GET"])
+def help_():
+    return flask.render_template("help.html")
 
 
 @app.route("/chatbot", methods=["POST"])
 def chatbot():
     if flask.request.method == "POST":
         user_input = flask.request.json["user_input"]
-        print(user_input)
         resp = chatbot_.respond(user_input)
         if not resp:
             resp = "Sorry, I don't understand. Please try again."
